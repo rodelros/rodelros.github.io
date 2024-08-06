10c08a9eb006d522c4c2d2811dda4fddc78e8729
Rodel Ros <rodel.ros@gmail.com>
2024-08-06 19:37:59 -0400

    added custom font and gtm

--- projects/flair/frontend/src/app/layout.tsx
+++ projects/flair/frontend/src/app/layout.tsx
@@ -1,22 +1,47 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import localFont from "next/font/local";
+import Header from "@/components/header/header";
+import { GoogleTagManager } from '@next/third-parties/google' 
 
 
 export const metadata: Metadata = {
   title: "Flair Airlines - Best prices on flights in Canada"
 };
 
+const averta = localFont({
+  src:[
+    {path: '../../public/fonts/averta/Averta-Regular.woff2', weight: '400', style: 'normal'},
+    {path: '../../public/fonts/averta/Averta-Bold.woff2', weight: '700', style: 'normal'},
+    {path: '../../public/fonts/averta/Averta-Black.woff2', weight: '900', style: 'normal'},
+    {path: '../../public/fonts/averta/Averta-Semibold.woff2', weight: '600', style: 'normal'},
+  ]
+})
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={averta.className}>
+      <GoogleTagManager gtmId="GTM-NZPX55P" />
+      
       <head>
         <link rel="icon" href="favicon.png" type="image/x-icon" />
       </head>
-      <body>{children}
+      <body>
+
+      <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZPX55P" 
+            height="0" 
+            width="0" 
+            style={{display: "none", visibility: "hidden"}}>
+          </iframe>
+        </noscript>
+
+
+        <Header />
+        {children}
       </body>
     </html>
   );
--- projects/flair/frontend/src/app/page.tsx
+++ projects/flair/frontend/src/app/page.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Averta } from "../../styles/fonts/fonts";
+
 
 
 export default function Home() {
   return (
     <>
-    <main className="${averta.className}">
-      <img src="flair_logo.svg" alt="Flair Logo"></img>
-      OK what
+    <main>
+      Let's see this then.
     </main>
     </>
   );
