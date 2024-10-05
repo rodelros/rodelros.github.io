cbd3654cbdf4ca2555cbfe4ef0ed2bc5c8052dcb
Rodel Ros <rodel.ros@gmail.com>
2024-10-05 17:09:54 -0400

    clean up

--- projects/flair/frontendv2/src/components/header/navigation/optional_fees/optional_fees.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './optional_fees.module.css';
-import commonstyles from '../../header.module.css';
-export default function OptionalFees() {
-    return (
-        <div className={commonstyles.nav_menu}>
-            <a href="/">optional fees</a>
-        </div>
-    );
-}
