ce12e3b8d46eb6ae411956ba4e06a6463318e335
Rodel Ros <rodel.ros@gmail.com>
2025-01-02 20:07:19 -0500

    added nextjs study codes

---
+++ projects/nextjs/study/src/app/adding_interactivity/page.tsx
@@ -0,0 +1,7 @@
+export default function AddingInteractivity() {
+    return (
+        <main>
+            Adding Interactivity
+        </main>
+    );
+}
--- projects/nextjs/study/src/app/flair/layout.tsx
+++ projects/nextjs/study/src/app/flair/layout.tsx
@@ -7,11 +7,7 @@ export const metadata: Metadata = {
   description: "Test Flair site",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children}: Readonly<{ children: React.ReactNode;}>) {
   return (
     <html lang="en">
       
