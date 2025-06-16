91273d1179eaab1b18cab6ade6f22f5972c67f4d
Rodel Ros <rodel.ros@gmail.com>
2025-06-15 20:46:30 -0400

    added composite strategy memoization

--- projects/dotnet/DesignPatterns/CompositeStrategy.cs
+++ projects/dotnet/DesignPatterns/CompositeStrategy.cs
@@ -8,21 +8,33 @@ public class Product(string Name, decimal Price)
     public string Name { get; } = Name;
     public decimal Price { get; } = Price;
 
+    private decimal _discountedPrice = 0;
+
     private List<DiscountStrategy> _discountStrategies = [];
     public void AddDiscount(DiscountStrategy discountStrategy)
     {
+        _discountedPrice = 0; // Reset discount price when adding a new strategy
         _discountStrategies.Add(discountStrategy);
     }
 
     public void RemoveDiscount(DiscountStrategy discountStrategy)
     {
+        _discountedPrice = 0; // Reset discount price when removing a strategy
         _discountStrategies.Remove(discountStrategy);
     }
 
     private decimal GetDiscountedPrice()
     {
+        if(_discountedPrice > 0)
+            return _discountedPrice; // Return cached discounted price if available
+
+        if (_discountStrategies.Count == 0)
+            return Price; // No discounts applied, return original price
+
+        // Calculate total discount from all strategies
         decimal totalDiscount = _discountStrategies.Sum(strategy => strategy(this));
-        return Math.Max(0, Price - Math.Min(totalDiscount, Price));
+        _discountedPrice = Math.Max(0, Price - Math.Min(totalDiscount, Price));
+        return _discountedPrice;
     }
     
     public decimal DiscountedPrice
